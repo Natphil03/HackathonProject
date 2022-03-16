f23fb44fa84c8d43058317cd7514abc22dd831e3
Matthew Hill <matthewjhillcs@gmail.com>
2022-03-16 07:01:25 +0000

    Start styling on header of index

---
+++ scripts/chartOption.js
@@ -0,0 +1,20 @@
+var chart1 = {
+    title: {
+        text: 'Carbon emissions over the last year'
+    },
+    tooltip: {},
+    legend: {
+        data: ['sales']
+    },
+    xAxis: {
+        data: ['Shirts', 'Cardigons', 'Chiffons', 'Pants', 'Heels', 'Socks']
+    },
+    yAxis: {},
+    series: [
+        {
+            name: 'sales',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }
+    ]
+};
---
+++ scripts/createCharts.js
@@ -0,0 +1,6 @@
+function initCharts() {
+    var myChart = echarts.init(document.getElementById('chart1'));
+
+    myChart.setOption(chart1)
+
+}
